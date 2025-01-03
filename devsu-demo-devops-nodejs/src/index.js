b70178c7a5491902e07c005fc3be3a0e7e53dcb0
EsmerlinJM <esmerlinjm@gmail.com>
2025-01-03 14:43:49 -0300

    [FEAT](init): Adding source code

---
+++ devsu-demo-devops-nodejs/src/index.js
@@ -0,0 +1,21 @@
+import sequelize from './shared/database/database.js'
+import { usersRouter } from "./users/router.js"
+import express from 'express'
+import * as dotenv from 'dotenv'
+dotenv.config()
+
+const app = express()
+const PORT = process.env.PORT
+
+sequelize.sync({ force: true }).then(() => console.log('db is ready'))
+
+app.use(express.json())
+app.use('/api/users', usersRouter)
+
+app.use('/ping', (req, res) => { res.status(200).json(`pong`)})
+
+const server = app.listen(PORT, () => {
+    console.log('Server running on port PORT', PORT)
+})
+
+export { app, server }
